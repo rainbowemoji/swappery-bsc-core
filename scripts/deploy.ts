27ef2ea24d6aecab375561913103c7d825299ae5
rainbowemoji <devzone210@gmail.com>
2022-02-10 03:46:43 +0900

    update core

---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const factory = await ethers.getContractFactory("PancakeFactory");
+  const pancakeFactory = await factory.deploy("0x2C0b73164AF92a89d30Af163912B38F45b7f7b65");
+
+  await pancakeFactory.deployed();
+
+  console.log("PancakeFactory deployed to:", pancakeFactory.address);
+}
+main().then(
+  ()=> process.exit(0)).catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
