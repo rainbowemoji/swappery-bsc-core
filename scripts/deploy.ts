e335d7e978e0a9ba70a7ccc0972a363c56a5638c
rainbowemoji <devzone210@gmail.com>
2022-02-12 06:07:41 +0900

    add deploy.ts file

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,12 +5,12 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const factory = await ethers.getContractFactory("PancakeFactory");
+  const factory = await ethers.getContractFactory("PancakeRouter");
   const pancakeFactory = await factory.deploy("0x2C0b73164AF92a89d30Af163912B38F45b7f7b65");
 
   await pancakeFactory.deployed();
 
-  console.log("PancakeFactory deployed to:", pancakeFactory.address);
+  console.log("PancakeRouter deployed to:", pancakeFactory.address);
 }
 main().then(
   ()=> process.exit(0)).catch((error) => {
